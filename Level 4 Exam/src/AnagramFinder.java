71ca75cd35e7b87ffcb406a213a139addefb6802
TheTrueTechie <shiva.malhotra.email@gmail.com>
2017-12-14 23:49:48 -0800

    ArrayFinder
    Final version

--- Level 4 Exam/src/AnagramFinder.java
+++ Level 4 Exam/src/AnagramFinder.java
@@ -1,51 +1,63 @@
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Stack;
 
 import javax.swing.JOptionPane;
 
 public class AnagramFinder {
+	String inputedWord;
 
-	public static void main(String[] args) throws IOException {
-		/*
-		ArrayList anagramSearcher = new ArrayList();
-		Scanner textFileReader = new Scanner(new File("AnagramList.txt"));
+	public static void main(String[] args) {
+		AnagramFinder a = new AnagramFinder();
+	}
 
-		String inputedWord = " ";
-		String listWord = " ";
-		String finalWord = " ";
+	public AnagramFinder() {
+		inputedWord = JOptionPane.showInputDialog("Hello! Please input an anagram. Thank You!");
+		run(inputedWord);
+	}
 
-		char inputedLetter = ' ';
-		char listLetter = ' ';
+	public String run(String word) {
+		Scanner textFileReader = null;
+		try {
+			textFileReader = new Scanner(new File("AnagramList.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while (textFileReader.hasNext()) {
+			int count = 0;
+			char letters1[] = word.toCharArray();
 
-		JOptionPane.showMessageDialog(null,
-				"Hello, please enter a word and we will search through our database with 235,866 words to see if there is an anagram!");
-		inputedWord = JOptionPane.showInputDialog("Enter here!");
+			String word1 = "";
+			word1 = textFileReader.nextLine();
 
-		for (int i = 0; i < 2358661; i++) {
-			anagramSearcher.add(textFileReader.nextLine());
-			anagramSearcher.get(0);
+			char letters2[] = word1.toCharArray();
 
-			listWord = anagramSearcher.toString();
-			listLetter = listWord.charAt(i);
+			int list1 = letters1.length;
+			int list2 = letters2.length;
 
-			if (inputedLetter == listLetter) {
-				JOptionPane.showMessageDialog(null, listWord.length());
+			if (list1 == list2) {
+
+				for (int i = 0; i < letters1.length; i++) {
+					for (int j = 0; j < letters1.length; j++) {
+						if (letters1[i] == letters2[j]) {
+							count++;
+							break;
+						}
+					}
+				}
+			}
+			if (count == letters1.length) {
+				if (!word1.equals(inputedWord)) {
+					System.out.println(word1);
+				}
+				if (count == 0) {
+					System.out.println("No Anagrams Detected");
+				}
 			}
 		}
-/*
-		/*
-		 * for (int i = 0; i < 235886; i++) {
-		 * anagramSearcher.add(textFileReader.nextLine()); anagramSearcher.get(i);
-		 * currentWord = anagramSearcher.toString(); currentLetter =
-		 * currentWord.charAt(i);
-		 * 
-		 * for (int j = 235886; j < args.length; j++) { anagramSearcher.get(j+1);
-		 * nextWord = anagramSearcher.toString(); nextLetter = nextWord.charAt(j); if
-		 * (currentLetter==nextLetter) { System.out.println(currentLetter + nextLetter);
-		 * } //System.out.println(anagramSearcher.size());
-		 * //System.out.println(textFileReader.nextLine()); } }
-		 */
+
+		return "No Anagrams Detected";
+
 	}
 }
